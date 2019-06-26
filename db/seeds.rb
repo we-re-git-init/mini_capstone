937058bbacece4176be1341a099702074ed6d9f2
Brian <riceb53@gmail.com>
2019-06-25 20:10:18 -0500

    initial commit, make model

---
+++ db/seeds.rb
@@ -0,0 +1,171 @@
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
+product = Product.new(name: "cookies", price: 4, image_url: "", description: "monsters love them")
+
+product.save
