6d9b838e4eb1da50067084c554b1273cdf8e04aa
Brian <riceb53@gmail.com>
2019-06-25 20:44:54 -0500

    add ffaker to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-product = Product.new(name: "muffins", price: 3, image_url: "", description: "i don't know")
 
-product.save
+100.times do
+  product = Product.new(price: rand(50), name: FFaker::Product.product, description: FFaker::HipsterIpsum.paragraph, image_url: FFaker::Image.url)
 
+  product.save
+end
