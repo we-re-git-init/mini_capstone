0c521bbddd771425803a11482fea37647aaf6ab8
Brian <riceb53@gmail.com>
2019-06-26 21:28:28 -0500

    show product with segment parameter id

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    # query param
     get '/product' => 'products#the_product'
+    # url segment
+    get '/product/:id' => 'products#the_product'
+
     get '/all_products' => 'products#all_products'
   #   get "/photos" => "photos#index"
   end
