24fc33498800fbcca0cd94a3bc3cce7c78fc6d43
Brian <riceb53@gmail.com>
2019-06-30 10:18:15 -0500

    products index and show actions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,12 +1,11 @@
 class Api::ProductsController < ApplicationController
-  def the_product
-    product_id = params["id"]
-    @product = Product.find_by(id: product_id)
-    render 'product.json.jb'
+  def index
+    @products = Product.all
+    render 'index.json.jb'
   end
 
-  def all_products
-    @products = Product.all
-    render 'all.json.jb'
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    # query param
-    get '/product' => 'products#the_product'
-    # url segment
-    get '/product/:id' => 'products#the_product'
-
-    get '/all_products' => 'products#all_products'
-  #   get "/photos" => "photos#index"
+    get '/products' => 'products#index'
+    get '/products/:id' => 'products#show'
   end
 end
