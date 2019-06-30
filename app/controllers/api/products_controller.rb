a72af34c77873db44e78ec945684577342c6c3ef
Brian <riceb53@gmail.com>
2019-06-30 11:39:51 -0500

    add products update action, with defaults when parameters are left blank

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -8,4 +8,15 @@ class Api::ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render 'show.json.jb'
   end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:input_name] || @product.name,
+      price: params[:input_price] || @product.price,
+      image_url: params[:input_image_url] || @product.image_url,
+      description: params[:input_description] || @product.description
+    )
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     get '/products' => 'products#index'
     get '/products/:id' => 'products#show'
+    # update
+    patch '/products/:id' => 'products#update'
   end
 end
