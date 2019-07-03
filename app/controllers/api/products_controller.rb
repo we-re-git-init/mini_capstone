e6a444c55c2d2e45d44a401717531a6433295b66
Brian <riceb53@gmail.com>
2019-07-02 19:27:30 -0500

    add model methods and show them in index.json.jb

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -9,13 +9,18 @@ class Api::ProductsController < ApplicationController
     render 'show.json.jb'
   end
 
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
   def update
     @product = Product.find_by(id: params[:id])
     @product.update(
-      name: params[:input_name] || @product.name,
-      price: params[:input_price] || @product.price,
-      image_url: params[:input_image_url] || @product.image_url,
-      description: params[:input_description] || @product.description
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      image_url: params[:image_url] || @product.image_url,
+      description: params[:description] || @product.description
     )
     render 'show.json.jb'
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price < 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def tax
+    tax_rate = 0.09
+    price * tax_rate
+  end
+
+  def total
+    price + tax
+  end
 end
