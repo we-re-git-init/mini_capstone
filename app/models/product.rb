9d7dc70626dbbd149e9000f9b4592d0daa8508af
Brian <riceb53@gmail.com>
2019-07-08 21:28:40 -0500

    replace model methods with has_many belongs_to

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,12 @@
 class Product < ApplicationRecord
+  belongs_to :supplier
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true
   validates :description, length: {in: 10..500 }
-
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
 
   def is_discounted?
     price < 10
